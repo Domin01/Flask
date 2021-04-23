25659436dad5f10cb611c177bba79e733eada5f7
Domin01 <alejandroomin@gmail.com>
2021-04-23 15:03:14 +0200

    He creado el segundo ejercicio

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 app = Flask(__name__)
 
 #La página principal debe mostrar una página donde ponga vuestro nombre y 
@@ -9,7 +9,6 @@ app = Flask(__name__)
 def inicio():
     return render_template("inicio.html")
 
-#Página potencia: Se accede con la URL /potencia/base/exponente (siendo base y exponente dos números enteros). Se muestra una página dinámica donde se muestra un título "Calcular potencia", se muestra la base y el exponente y se muestra el resultado:
 #Si el exponente es positivo, el resultado es la potencia.
 #Si el exponente es 0, el resultado es 1.
 #Si el exponente es negativo, el resultado es 1/potencia con el exponente positivo.
@@ -18,8 +17,15 @@ def inicio():
 def potencia():
     return render_template("potencia.html")
 
+#Página potencia: Se accede con la URL /potencia/base/exponente (siendo base y exponente dos números enteros). 
+#Se muestra una página dinámica donde se muestra un título "Calcular potencia", se muestra la base y el exponente 
+#y se muestra el resultado:
+
 @app.route('/potencia/<int:num1>/<num2>',methods=["GET","POST"])
 def calcularpotencia(num1,num2):
+#Si el exponente es positivo, el resultado es la potencia.
+#Si el exponente es 0, el resultado es 1.
+#Si el exponente es negativo, el resultado es 1/potencia con el exponente positivo.
     base=num1
     exponente=int(num2)
     if exponente>0:
@@ -31,22 +37,29 @@ def calcularpotencia(num1,num2):
     return render_template("potencia.html",num1=base,num2=exponente,res=resultado)
 
 #Página cuenta letras: Se accede con la URL /cuenta/palabra/letra (siendo palabra y letra dos cadenas cualquiera). 
-#Si la letra no es una cadena con un carácter se devuelve un error 404. 
-#Se muestra una página donde hay un título "Cuanta letras", 
-#y muestra el siguiente mensaje "En la palabra ********* aparece *** veces el carácter ***".
 
 @app.route('/letras')
 def letras():
     return render_template("letras.html")
 
+#Página cuenta letras: Se accede con la URL /cuenta/palabra/letra (siendo palabra y letra dos cadenas cualquiera).
+@app.route('/cuenta/<dato1>/<dato2>',methods=["GET","POST"])
+def contar(dato1,dato2):
+#Si la letra no es una cadena con un carácter se devuelve un error 404.
+    palabra=dato1
+    letra=dato2
+    if len(letra) > 1:
+        abort(404)
+#muestra el siguiente mensaje "En la palabra ********* aparece *** veces el carácter ***".
+    else:
+        resultado=palabra.count(letra)
 
+    return render_template("letras.html",dato1=palabra,dato2=letra,res=resultado)
 
+#Página libros: A esta página se entra con la URL /libro/codigo (siendo código un número entero). 
 
-
-
-
-
-
+Tienes que buscar el código en el fichero libros.xml y debes mostrar una página con un título "Biblioteca" 
+y el nombre del libro y el autor. Si no existe el código debe devolver una respuesta 404.
 
 @app.route('/libros')
 def libros():
